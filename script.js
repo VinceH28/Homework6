5e8bbf836b516113c914b63d967edf7551269c47
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-08 15:19:34 -0400

    updated files

--- script.js
+++ script.js
@@ -3,36 +3,36 @@ const form = document.querySelector("#searchInput");
 
 //Variables
 const apiKey = '093063c871e4d4f901ab6cb1072ea5e4';
-const searchHistroy = [];
-const currentDay = luxon.DateTime.local().toLocaleString({
+const userSearch = [];
+const today = luxon.DateTime.local().toLocaleString({
     weekday: "short",
     month: "short",
     day: "2-digit",
   }); 
 
-//API call for City Search
+//API call for City Weather Search Resuls
 const todaysWeather = async (citySearch) => {
   const results = await axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=${apiKey}`
   );
   $("#searchContent").css("display", "block");
   $("#searchDetails").empty();
-  const weatherData = results.data;
-  const weatherImage = weatherData.weather[0].icon;
+  const apiResponse = results.data;
+  const weatherImage = apiResponse.weather[0].icon;
   const imageURL = `https://openweathermap.org/img/w/${weatherImage}.png`;
 
   //Attach data to page
-  let citySearchElement = $(`
+  let usercitySearch = $(`
     <h1 id ='cityName'>
-    ${weatherData.name} (${today}) <img src='${imageURL}'/>
+    ${apiResponse.name} (${today}) <img src='${imageURL}'/>
     </h1>
-    <p> Temp: ${weatherData.main.temp} °F </p>
-    <P> Humidity: ${weatherData.main.temp} %</p>
-    <p> Wind Speed: ${weatherData.main.temp} MPH</p>
+    <p> Temp: ${apiResponse.main.temp} °F </p>
+    <P> Humidity: ${apiResponse.main.temp} %</p>
+    <p> Wind Speed: ${apiResponse.main.temp} MPH</p>
   `);
-  $('#searchDetails').append(citySearchElement);
-  const latitude = weatherData.coord.lat;
-  const longitude = weatherData.coord.lon;
+  $('#searchDetails').append(usercitySearch);
+  const latitude = apiResponse.coord.lat;
+  const longitude = apiResponse.coord.lon;
   
   //UV Index
   axios
@@ -46,60 +46,66 @@ const todaysWeather = async (citySearch) => {
       </p>
       `);
       $('#searchDetails').append(uvIndexEl);
-      fivedayForecast(latitude, longitude);
+      fivedayapiData(latitude, longitude);
       //Index Colors
-      if (uvIndex < 2) {
+      if (uvIndex >= 2) {
         uvIndex.classList.add("uvIndexGreen")
         $('Low');
-      } else if (uvIndex < 5) {
+      } else if (UVindex > 2 && UVindex <= 5) {
+        uvIndex.classList.add("uvIndexGreen")
+        $('Low');
+      }
+      else if (UVIndex > 5 && UVIndex <= 7) {
         uvIndex.classList.add("uvIdndexYellow")
         $('Moderate');
       } else if (uvIndex < 7) {
         uvIndex.classList.add("uvIndexOrange")
         $('High');
-      } else if (uvIndex < 10) {
+      } else if (uvIndex >=7 && uvIndex <= 10) {
         uvIndex.classList.add("uvIndexRed")
         $('Very High');
-      } else {
-        uvIndex.classList.add("ultraviolet");
-      }
+      } else if (uvIndex < 10)  {
+        uvIndex.classList.add("ultraviolet")
+        $('Very High');
+      } 
+
     });
 };
 
 //Five Day Forecast
-function fivedayForecast(latitude, longitude) {
+function fivedayapiData(latitude, longitude) {
     axios
     .get(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`
     )
-    .then(function (fivedayForecastRes) {
-      $('#fivedayForecast').empty();
+    .then(function (fivedayapiDataRes) {
+      $('#fivedayapiData').empty();
       for (let i = 1; i <6; i++) {
-        let forecastWeatherInfo = {
-          date: fivedayForecastRes.data.daily[i].dt,
-          temp: fivedayForecastRes.data.temp.day,
-          image: fivedayForecastRes.data.weather[0].image,
-          humidity: fivedayForecastRes.data.daily[i].humidity,
+        let apiWeatherInfo = {
+          date: fivedayapiDataRes.data.daily[i].dt,
+          temp: fivedayapiDataRes.data.temp.day,
+          image: fivedayapiDataRes.data.weather[0].image,
+          humidity: fivedayapiDataRes.data.daily[i].humidity,
         };
         let forecastCurrentDate = luxon.DateTime.fromSeconds(
-          forecastWeatherInfo.date
+          apiWeatherInfo.date
         ).toLocaleString({ weekday: 'short', month: 'short', day: '2-digit' });
-        let forecastImage = `<img src= 'https://openweathermap.org/img/w/${forecastWeatherInfo.icon}.png' />`;
+        let forecastImage = `<img src= 'https://openweathermap.org/img/w/${apiWeatherInfo.icon}.png' />`;
         
         //5 Day Cards
-        forecastWeatherCard = $(`
+        WeatherCard = $(`
         <div class="p-3">
         <div class="card text-light bg-transparent">
           <div class="card-body">
             <p>${forecastCurrentDate}</p>
             <p>${forecastImage}</p> 
-            <p>Temperature: ${forecastWeatherInfo.temp} °F</p>
-            <p>Humidity: ${forecastWeatherInfo.humidity} %</p>
+            <p>Temperature: ${apiWeatherInfo.temp} °F</p>
+            <p>Humidity: ${apiWeatherInfo.humidity} %</p>
           </div>
         </div>
       </div>
     `);
-    $('#fiveDay').append(forecastWeatherCard);
+    $('#fiveDay').append(WeatherCard);
       }
     });
 }
@@ -112,15 +118,15 @@ form.addEventListener('submit', function (event) {
   
   let citySearch= form.elements.query.value.trim();
   currentWeather(citySearch);
-  if (!searchHistroy.includes(citySearch)) {
-    searchHistroy.push(citySearch);
-    let citySearchElement = $(`
+  if (!userSearch.includes(citySearch)) {
+    userSearch.push(citySearch);
+    let usercitySearch = $(`
     <li class="list-group-item bg-transparent text-light text-center pointer history">${citySearch}</li>
     `);
     $('#searchHistory').append(citySearch);
   }
 
-  localStorage.setItem('citySearch', JSON.stringify(searchHistroy));
+  localStorage.setItem('citySearch', JSON.stringify(userSearch));
   form.elements.query.value = '';
 });
 // 2. List
@@ -135,7 +141,7 @@ $(document).ready(function () {
 
   if (citySearchHistory !== null) {
     let lastSearchElment = citySearchHistory.length -1;
-    let lastcitySearchElement = citySearchHistory[lastSearchElment];
-    currentWeather(lastcitySearchElement)
+    let lastuserSearch = citySearchHistory[lastSearchElment];
+    currentWeather(lastuserSearch);
   }
 });
